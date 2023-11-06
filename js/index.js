7ba05a88280afc74c5a679856a402fe49acbf526
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-11-06 14:32:27 -0300

    ejercicios teóricos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+/*
+    Ejercicio 1
+    Tengo desarrollada la siguiente función en javascript
+    function suma (a,b,callback){
+    let c = a +b;
+    callback();
+    }
+    ¿Que es el tercer parámetro recibido?
+
+    El tercer parámetro recibido es una función
+
+    ¿En que casos es obligatorio desarrollar este tipo de funciones?
+
+    Es obligatorio en los casos que se necesite llamar una función extra para realizar un procedimiento
+    y en los métodos nativos que requieran una callback
+
+    Realizar un llamado a la función de ejemplo
+
+     suma(1, 2, callback);
+*/
+
+/*
+    Ejercicio 2
+    
+    Desarrollador una calculadora que tenga:
+    ● 2 campos inputs para los operadnos
+    ● 4 botones de operadores básicos (suma, resta, multiplicación, división)
+    Al realizar la cuenta se deberá actualizar el campo resultado
+*/
+
 //constantes y variables
 const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
